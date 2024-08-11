1b124f0acfc7b66738eec1baf5ee1248e013e789
Justin Watson <justinwatson651@gmail.com>
2024-08-11 17:55:20 +0000

    Created fact table for genre sales by region and top selling genres, staging layers for distinct consoles and refactored video game sales staging layer

--- models/staging/stg_video_game_sales.sql
+++ models/staging/stg_video_game_sales.sql
@@ -17,21 +17,8 @@ with raw_sales as (
         release_date,
         last_update
     from {{ source('RAW_VIDEO_GAME_SALES_DATA', 'DATA') }}
+    where total_sales is not null
 )
 
-select
-    title,
-    console as platform,
-    genre,
-    publisher,
-    developer,
-    critic_score,
-    total_sales,
-    na_sales,
-    jp_sales,
-    pal_sales,
-    other_sales,
-    release_date,
-    last_update
-from raw_sales
-where total_sales is not null
+
+select * from raw_sales
