1be087c32ad4a9cc3e77a2af86689a99969d81b5
Justin Watson <justinwatson651@gmail.com>
2024-08-12 19:04:29 +0000

    Refactored int layer for sales by date and created fact table for sales by quarter

--- models/intermediate/release_date/int_sales_by_date.sql
+++ models/intermediate/release_date/int_sales_by_date.sql
@@ -2,11 +2,12 @@
 
 with sales_by_release_date as (
   select
-    rd.release_date,
+    s.release_date,
     sum(s.total_sales) as total_sales
   from {{ ref('stg_release_date') }} as rd
-  left join {{ ref('stg_video_game_sales') }} as s on rd.release_date = s.release_date
-  group by rd.release_date
+  inner join {{ ref('stg_video_game_sales') }} as s on rd.release_date = s.release_date
+  group by s.release_date
+  order by s.release_date desc
 )
 
 select * from sales_by_release_date
